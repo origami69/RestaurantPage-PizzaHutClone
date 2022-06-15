9b38709d07371f11f9888b287283edd180c9d19a
origami <abecerra6767@gmail.com>
2022-06-15 15:42:22 -0700

    adding project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import './sass/styles.scss'
+
+
+import { myPage, theActivators } from './myName.js';
+
+function component() {
+    myPage
+    theActivators
+}
+
+component()
+
+
+//this is the multi function of exporting
+
+// const functionOne = () => 'ONE';
+// const functionTwo = () => 'TWO';
+
+// export {
+//     functionOne,
+//     functionTwo
+// };
